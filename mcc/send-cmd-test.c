303f496caeea26c97bdf1485313b61bb5aad9031
ivzvvv <ivancojoc20@gmail.com>
2024-05-28 17:33:23 +0100

    send-cmd-test.c cleanup

--- mcc/send-cmd-test.c
+++ mcc/send-cmd-test.c
@@ -14,8 +14,8 @@
 #include <signal.h>
 #include <arpa/inet.h>
 
-#define SERVER_ADDRESS "127.0.0.1"  // Localhost IP address
-#define SERVER_PORT 9090               // Port to connect to
+#define SERVER_ADDRESS "127.0.0.1"  
+#define SERVER_PORT 9090               
 
 int main(int argc, char *argv[]) {
     int sockfd;
@@ -26,62 +26,52 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    char delimiter = argv[1][0];  // Extract the first character as delimiter
+    char command = argv[1][0];  
     int value = 0; 
 
     if (argc == 3){
-        value = atoi(argv[2]);     // Convert the second argument to integer
+        value = atoi(argv[2]);     
     }
     
-    // Create the formatted string with appropriate size
-    int max_value_length = 12;  // Assuming maximum integer value length is 10
+    int max_value_length = 12;  
     int string_length =0;
-    if(value != 0)
-        string_length = 3 + 1 + strlen(argv[2]) + max_value_length + 2;  // Delimiters, colon, and null terminator
+    if(command != 'H')
+        string_length = 3 + 1 + strlen(argv[2]) + max_value_length + 2;  
     else
-        string_length = 3 + 1 + max_value_length + 2;  // Delimiters, colon, and null terminator
+        string_length = 3 + 1 + max_value_length + 2;  
     char formatted_string[string_length];
 
-    // Format the string using sprintf
-    if (value == 0)
-        snprintf(formatted_string, string_length, ":%c:", delimiter);
+    if (command == 'H')
+        snprintf(formatted_string, string_length, ":%c:", command);
     else
-        snprintf(formatted_string, string_length, ":%c:%d:", delimiter, value);
+        snprintf(formatted_string, string_length, ":%c:%d:", command, value);
 
     printf("Formatted string: %s\n", formatted_string);
     printf("length/max length = %li/%i\n", strlen(formatted_string), string_length);
 
-    // Create a socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);  // TCP socket
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);  
     if (sockfd < 0) {
     perror("socket");
     exit(1);
     }
-
-    // Set up server address information
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
     server_addr.sin_port = htons(SERVER_PORT);
-
-    // Connect to the server
     if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-    perror("connect");
-    close(sockfd);
-    exit(1);
+        perror("connect");
+        close(sockfd);
+        exit(1);
     }
-
-    // Send data to the server
     int bytes_sent = send(sockfd, formatted_string, strlen(formatted_string), 0);
     if (bytes_sent < 0) {
-    perror("send");
-    close(sockfd);
-    exit(1);
+        perror("send");
+        close(sockfd);
+        exit(1);
     }
 
-    printf("Sent %d bytes of data to the server.\n", bytes_sent);
-
-    // Close the socket
+    printf("Sent %d bytes of data to OBC-P1.\n", bytes_sent);
     close(sockfd);
 
     return 0;
